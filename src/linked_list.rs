f1296dfe249148da91387f17d7c9d5e139ce1a44
Regis <boudinot.regis@yahoo.com>
2016-07-01 14:27:51 -0600

    append / tail / count

--- src/linked_list.rs
+++ src/linked_list.rs
@@ -7,25 +7,30 @@ pub struct LinkedList {
 
 impl LinkedList {
     pub fn new() -> LinkedList {
-        LinkedList {
-            head: Node::new(),
-        }
+        LinkedList { head: Node::new() }
     }
 
-    pub fn tail(&mut self, node: Option<Box<Node>>) -> &mut Node {
-        match node {
-            Some(f) => {
-                if f.next_node == None {
-                    let current_node = &mut self.head;
-                    current_node
-                } else {
-                    self.tail(f.next_node)
-                }
-            },
-            None => {
-                self.tail(Some(Box::new(Node::new())))
-            }
+    pub fn count(&mut self) -> usize {
+        let mut count = 0;
+        let mut current_node = &self.head;
+
+        while let Some(ref f) = current_node.next_node {
+            
+            current_node = f;
+            count += 1;
         }
+
+        count
+    }
+
+    pub fn append(&mut self, data: Option<String>) {
+        let mut new_node = Node::new();
+        new_node.data = data;
+        self.head.tail().next_node = Some(Box::new(new_node));
+    }
+
+    pub fn tail(&mut self) -> &mut Node {
+        self.head.tail()
     }
 }
 
@@ -40,8 +45,18 @@ fn linked_list_intializes() {
 #[test]
 fn it_can_find_tail() {
     let mut new_list = LinkedList::new();
-    let mut test_list = LinkedList::new();
+
+    new_list.head.data = Some("hello".to_string());
 
     assert_eq!(None, new_list.head.next_node);
-    assert_eq!(&mut test_list.head, new_list.tail(None));
+    assert_eq!(Some("hello".to_string()), new_list.tail().data);
+}
+
+#[test]
+fn it_can_append_data() {
+    let mut new_list = LinkedList::new();
+
+    new_list.append(Some("wow".to_string()));
+
+    assert_eq!(Some("wow".to_string()), new_list.tail().data)
 }
--- src/node.rs
+++ src/node.rs
@@ -1,16 +1,24 @@
 #[derive(Debug, PartialEq)]
 pub struct Node {
-    pub data: Option<String>,  
-    pub next_node: Option<Box<Node>>, 
+    pub data: Option<String>,
+    pub next_node: Option<Box<Node>>,
 }
 
 impl Node {
     pub fn new() -> Node {
         Node {
-            data: None, 
+            data: None,
             next_node: None,
         }
-    } 
+    }
+
+    pub fn tail(&mut self) -> &mut Node {
+        match self.next_node {
+            Some(ref mut f) => f.tail(),
+            None => self,
+        }
+    }
+    
 }
 
 #[test]
