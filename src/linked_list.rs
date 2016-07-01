955e71abe0ff7637224ed3c158580fe75921ea5d
selfup <boudinot.regis@yahoo.com>
2016-07-01 02:09:21 -0600

    tail works

--- src/linked_list.rs
+++ src/linked_list.rs
@@ -11,6 +11,22 @@ impl LinkedList {
             head: Node::new(),
         }
     }
+
+    pub fn tail(&mut self, node: Option<Box<Node>>) -> &mut Node {
+        match node {
+            Some(f) => {
+                if f.next_node == None {
+                    let current_node = &mut self.head;
+                    current_node
+                } else {
+                    self.tail(f.next_node)
+                }
+            },
+            None => {
+                self.tail(Some(Box::new(Node::new())))
+            }
+        }
+    }
 }
 
 #[test]
@@ -19,4 +35,13 @@ fn linked_list_intializes() {
 
     assert_eq!(None, new_list.head.data);
     assert_eq!(None, new_list.head.next_node);
+}
+
+#[test]
+fn it_can_find_tail() {
+    let mut new_list = LinkedList::new();
+    let mut test_list = LinkedList::new();
+
+    assert_eq!(None, new_list.head.next_node);
+    assert_eq!(&mut test_list.head, new_list.tail(None));
 }
